9036c55e7c85b359013c42898f10d2cb4b0953ad
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-10 16:38:06 +0200

    add task03

---
+++ selfuducation/Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] myArray = { 10, 5, 7, 6 };
+int lowest = myArray[0];
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] < lowest)
+    {
+        lowest = myArray[i];
+        Console.WriteLine(lowest);
+    }
+
+}
+
