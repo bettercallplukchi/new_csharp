1b2e954058c8e232821ffd17e365404d000150e6
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-10 16:37:46 +0200

    add task02

---
+++ selfuducation/Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] myArray = { 10, 5, 7, 6 };
+int sum = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    {
+        sum += myArray[i];
+        Console.WriteLine(sum);
+    }
+}
