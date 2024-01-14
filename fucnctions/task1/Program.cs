a59b42f333f8b5cb3bdb28804a71c716ffdcba16
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-14 13:21:55 +0200

    ADD first task

---
+++ fucnctions/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double res = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(res);
