5bbde297870052a0f30e6e1f375c2073e0e51539
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-29 20:42:06 +0200

    Добавил третье задание

---
+++ dz290124/task03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем произвольную строку
+        string inputString = "level";
+
+        // Проверяем, является ли строка палиндромом
+        bool isPalindrome = IsPalindrome(inputString);
+
+        // Выводим результат
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом.");
+        }
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        // Приводим строку к нижнему регистру для учета регистронезависимости
+        input = input.ToLower();
+
+        // Получаем обратную строку
+        string reversedString = ReverseString(input);
+
+        // Сравниваем исходную строку с обратной
+        return input == reversedString;
+    }
+
+    static string ReverseString(string input)
+    {
+        // Преобразуем строку в массив символов, обращаем массив и создаем новую строку
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
