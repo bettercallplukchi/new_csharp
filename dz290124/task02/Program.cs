fad6eae7ad81ff305b6a59263d196d1353ecb582
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-29 20:40:18 +0200

    Добавил второе дз

---
+++ dz290124/task02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем строку с латинскими буквами в обоих регистрах
+        string inputString = "Hello World";
+
+        // Формируем строку, в которой все заглавные буквы заменены на строчные
+        string resultString = ConvertToUpperToLower(inputString);
+
+        // Выводим результат
+        Console.WriteLine(resultString);
+    }
+
+    static string ConvertToUpperToLower(string input)
+    {
+        // Используем метод ToLower для замены всех заглавных букв на строчные
+        return input.ToLower();
+    }
+}
+
