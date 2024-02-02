42a5dd6f5fa42ffb99942db0266560411a28fa66
Sasha Plukchi <loxpojizniia@gmail.com>
2024-02-02 11:09:59 +0200

    Добавил первое итоговое дз

---
+++ DZ01_02_2024/task01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbers(m, n);
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write($"{m} ");
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+}
+
