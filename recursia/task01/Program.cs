689e0e3b3c185b26050a946bc7d1821b8514fc42
Sasha Plukchi <loxpojizniia@gmail.com>
2024-02-02 11:06:39 +0200

    Добавил урок по рекурсии

---
+++ recursia/task01/Program.cs
@@ -0,0 +1,8 @@
+﻿int sum(int numbers)
+{
+    if (numbers == 0)
+        return 0;
+    return sum(numbers / 10) + numbers % 10;
+}
+Console.WriteLine(sum(123));
+
---
+++ recursia/task02/Program.cs
@@ -0,0 +1,20 @@
+﻿//// 3. Считать строку с консоли, содержащую латинские буквы.
+//    Вывести на экран согласные буквы этой строки.
+//    
+//    Использовать рекурсию. Не использовать цикл
+
+
+void str(string result, int index = 0)
+{
+    if (result.Length == index) return;
+
+    string vowels = "bcdfghjklmnpqrstvwxz";
+    if (vowels.Contains(char.ToLower(result[index])))
+    {
+        Console.Write(result[index]);
+    }
+    str(result, index + 1);
+}
+
+
+str("Hello world");
