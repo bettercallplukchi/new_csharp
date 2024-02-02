642cd2791e1807ad8ce82e6f5c152933268bdaa6
Sasha Plukchi <loxpojizniia@gmail.com>
2024-02-02 11:13:12 +0200

    Добавил третье итоговое дз

---
+++ DZ01_02_2024/task03/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Произвольный массив
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write($"{arr[index]} ");
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
+}
